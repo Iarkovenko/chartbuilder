b9eaf95e801cbb55c6e9ba439cf0bf3e8e41c673
Iarkovenko <yarkovenko@ukr.net>
2019-03-18 20:02:10 +0200

    fixed bugs with create crossfilter in 2 place

--- src/modules/moduleChartContainer.js
+++ src/modules/moduleChartContainer.js
@@ -1,7 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React, { Component, Fragment } from 'react';
-import * as d3 from 'd3';
-import crossfilter from 'crossfilter2';
 
 import SelectBarView from './selectBar/selectBarView';
 import Dashboard from './dc/dashboard';
@@ -10,13 +8,6 @@ class ChartContainer extends Component {
   state = {
     parametr: null
   };
-
-  componentDidMount() {
-    d3.csv('./data.csv', (err, data) => {
-      if (err) throw err;
-      this.ndx = crossfilter(data);
-    });
-  }
 
   onHandleChange = ({ target }) => {
     const { name, value } = target;
@@ -32,11 +23,7 @@ class ChartContainer extends Component {
           parametr={parametr}
           onHandleChange={this.onHandleChange}
         />
-        {parametr ? (
-          <Dashboard classes={classes} {...this.state}>
-            <h1>asdasd</h1>
-          </Dashboard>
-        ) : null}
+        {parametr ? <Dashboard classes={classes} {...this.state} /> : null}
       </Fragment>
     );
   }
